7780421d9647ac480c1a3c873232fa43f2e78e17
changdae <donstop9@gmail.com>
2023-01-16 14:11:25 +0900

    2023-01-16

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 with st.form("my_form", clear_on_submit=True):
    st.info('다음 양식을 모두 작성후 제출합니다.')
    uid=st.text_input('아이디',max_chars=12)
+   submitted = st.form_submit_button('제출')
+   if submitted:
+      st.success('제출됨')
