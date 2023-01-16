8e78bcb035a4979cc17700a33e1d1b9a42a8fdfa
changdae <donstop9@gmail.com>
2023-01-16 14:27:46 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 with st.form("my_form", clear_on_submit=True):
    st.info('다음 양식을 모두 작성후 제출합니다.')
    uid=st.text_input('아이디',max_chars=12)
+   uName = st.text_input('성명',max_chars=10)
    submitted = st.form_submit_button('제출')
+
    if submitted:
       st.success('제출됨')
