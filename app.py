cc0d0d69984140ddc90739be414ed330bace3d43
changdae <donstop9@gmail.com>
2023-01-16 14:27:46 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+from datetime import datetime
+
 import streamlit as st
+import datetime
+
 
 with st.form("my_form", clear_on_submit=True):
    st.info('다음 양식을 모두 작성후 제출합니다.')
    uid=st.text_input('아이디',max_chars=12)
+   uName = st.text_input('성명',max_chars=10)
+   uPw = st.text_input('비밀번호', type='password')
+   uPwChk = st.text_input('비밀번호확인', type='password')
+   uBirth = st.date_input('생년월일', min_value=datetime.date(1971,1,1))
    submitted = st.form_submit_button('제출')
+
    if submitted:
       st.success('제출됨')
